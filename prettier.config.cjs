0c900ffc34877674b83c701f43e84abdb9a59604
nateachino <natealleyne88@gmail.com>
2023-06-22 16:38:50 -0400

    Added prettier rules

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
     plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: "es5",
+    bracketSameLine: false,
 };
 
 module.exports = config;
